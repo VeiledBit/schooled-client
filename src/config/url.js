c197669d66750345111c9872037260500f25cf94
VeiledBit <12989770+VeiledBit@users.noreply.github.com>
2023-08-13 05:56:38 +0200

    Remove unneeded .env variable

--- src/config/url.js
+++ src/config/url.js
@@ -1,8 +1,8 @@
 /* eslint-disable operator-linebreak */
 const baseUrl =
-  process.env.NODE_ENV === "development" ? "http://localhost:3001" : process.env.BASE_URL;
+  process.env.NODE_ENV === "development" ? "http://localhost:3001" : process.env.REACT_APP_BASE_URL;
 
 const inviteUrl =
-  process.env.NODE_ENV === "development" ? "http://localhost:3000" : process.env.INVITE_URL;
+  process.env.NODE_ENV === "development" ? `${window.location.hostname}:3000` : window.location.hostname;
 
 export { baseUrl, inviteUrl };
